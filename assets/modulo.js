d59762c7e8cc39ffe05c466973bfeaae13c2d330
PericlesSavio <pericles.marques@outlook.com>
2021-07-27 18:55:41 -0300

    fix

--- assets/modulo.js
+++ assets/modulo.js
@@ -3,7 +3,7 @@ $(function () {
                 desenho_013, desenho_014, desenho_015, desenho_016];
    
   $.each(item, function (index, element) {
-    var Container = "<div class='box'><img src='imagens/_desconhecido.png' id='"+element.a+"_img'>"+
+    var Container = "<div class='box'><img src='imagens/desconhecido.png' id='"+element.a+"_img'>"+
       "<input id='"+element.a+"_input' class='input' placeholder='Qual é o desenho?' onkeyup='resposta("+element.a+".resposta, "+element.a+".input, "+element.a+".img, "+element.a+".arte, "+element.a+".nome)'>"+
       "<div class='float_l'><button class='btn btn-dark' value='audios/"+element.a+".mp3'>Tocar</button></div><div class='px10'></div><div class='float_r'>"+
       "<button class='btn btn-dark' onclick='javascript:stopAllAudio()'>Parar</button></div></div>"
